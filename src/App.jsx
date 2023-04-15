c056a82b8061d27caac12726cb91e4b1dcc2de83
RCornidez <cornidez04@gmail.com>
2023-04-15 23:55:48 +0000

    Added foundation components to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+//import styling
 import './App.css'
 
+const data = {
+	labels: ['Red', 'Yellow', 'Blue', 'Green', 'Purple', 'Orange'],
+  	datasets: [
+    	{
+      		data: [10, 10, 10, 10, 10, 50],
+      		backgroundColor: ['red', 'yellow', 'blue', 'green', 'purple', 'orange'],
+      		hoverBackgroundColor: ['red', 'yellow', 'blue', 'green', 'purple', 'orange']
+    	}
+  	]
+  };
+
+const options = {
+  	maintainAspectRatio: true
+};
+
+
+function ColorBlock(props) {
+
+const styles = {
+	backgroundColor: props.color,
+	fontSize: '36px',
+	height: '56px',
+	width: '56px',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	borderRadius: '50%',
+	margin: '10px'
+};
+
+
+return (
+	<div className="color-block" style={styles}>
+	<p>{props.emoji}</p>
+	</div>
+
+)
+
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+	<div className="app-wrapper">
+    <div className="vote" style={{display: 'block'}}>
+		<p style={{fontSize:'24px'}}>Select your favorite</p>
+		<div className="color-list">
+			<div className="color-sub-list">
+				<ColorBlock color="red" emoji="🤯"/>	
+				<ColorBlock color="yellow" emoji="😀"/>	
+				<ColorBlock color="blue" emoji="😥"/>	
+			</div>
+			<div className="color-sub-list">
+				<ColorBlock color="green" emoji="🤮"/>	
+				<ColorBlock color="purple" emoji="😴"/>	
+				<ColorBlock color="orange" emoji="😳"/>	
+			</div>
+
+		</div>
     </div>
+	<div className="results" style={{display: 'block'}}>
+		<p style={{fontSize:'24px'}}>Results</p>
+		<Pie data={data} options={options} />
+	</div>
+	
+	</div>
   )
 }
 
