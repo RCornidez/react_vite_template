caea71204a10f06a853ddcb8aa9bbc59815586b9
RCornidez <cornidez04@gmail.com>
2023-05-10 05:26:24 +0000

    Added react-query and updated chart data inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import { useState, useEffect } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import { customAlphabet } from "nanoid";
+import { useQuery, useMutation } from 'react-query';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 //import styling
 import './App.css'
 
+//setup chart
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 //user id setup
 const alphabet = '0123456789';
-const nanoid = customAlphabet(alphabet, 36);
+const nanoid = customAlphabet(alphabet, 10);
 
-
+/*
 //chart setup
-const data = {
+const chart_data = {
 	labels: ['Red', 'Yellow', 'Blue', 'Green', 'Purple', 'Orange'],
   	datasets: [
     	{
@@ -30,12 +32,13 @@ const data = {
 const options = {
   	maintainAspectRatio: true
 };
-
+*/
 
 //color and emoji setup
 function ColorBlock(props) {
 	const handleClick = () => {
 		localStorage.setItem('color', props.color);
+		props.send_vote(localStorage.getItem('user_id'), props.color);
 		props.click();
 	}
 	const styles = {
@@ -68,7 +71,6 @@ function App() {
 	const [showColor, setShowColor] = useState('block')
 	const [showResults, setShowResults] = useState()
 
-
 	//changes visibility of color choices
 	const changeVisibility = () => {
 		setShowColor('none');
@@ -96,27 +98,80 @@ function App() {
         }
 
   	}, [showColor]);
+
+	//HTTP Requests
+	//POST new vote
+  	const {mutate, isLoading, data, error } = useMutation(async (newItem) => {
+    	const response = await fetch('https://api.cornidez.com/new_vote', {
+      		method: 'POST',
+      		headers: {
+       			'Content-Type': 'application/json',
+    		},
+      		body: JSON.stringify(newItem),
+    	});
+    	console.log(response.json());
+  	});
+
+  	const AddItem = (id, vote) => {
+    	const newItem = {
+      		user_id: parseInt(id, 10),
+      		color: vote,
+    	};
+    	mutate(newItem);
+	};
+
+	//GET results
+	const get_query = useQuery('results', async () => {
+    	const response = await fetch('https://api.cornidez.com/results');
+    	return response.json();
+  	});
+
+	//adjust response json
+	const colors = ['red', 'yellow', 'blue', 'green', 'purple', 'orange'];
+	const sample = [0, 0, 0, 0, 0, 0];	
+	const counts = colors.map((color) => {
+		const colorObj = get_query.data && get_query.data.find((item) => item.color === color);
+		return colorObj ? parseInt(colorObj.count) : 0;
+	});
+
+	//CHART setup
+	const chart_data = {
+    	labels: ['Red', 'Yellow', 'Blue', 'Green', 'Purple', 'Orange'],
+    	datasets: [
+        	{
+            	data: get_query.isLoading ? sample : counts,
+            	backgroundColor: colors,
+            	hoverBackgroundColor: colors
+        	}
+    	]
+  	};
+
+	const options = {
+  		maintainAspectRatio: true
+	};
+
+
   	return (
 		<div className="app-wrapper">
     	<div className="vote" style={{display: showColor}}>
 			<p style={{fontSize:'24px'}}>Select your favorite</p>
 			<div className="color-list">
 			<div className="color-sub-list">
-				<ColorBlock color="red" emoji="🤯" click={changeVisibility}/>	
-				<ColorBlock color="yellow" emoji="😀" click={changeVisibility}/>	
-				<ColorBlock color="blue" emoji="😥" click={changeVisibility}/>	
+				<ColorBlock color="red" emoji="🤯" click={changeVisibility} send_vote={AddItem}/>	
+				<ColorBlock color="yellow" emoji="😀" click={changeVisibility} send_vote={AddItem}/>	
+				<ColorBlock color="blue" emoji="😥" click={changeVisibility} send_vote={AddItem}/>	
 			</div>
 			<div className="color-sub-list">
-				<ColorBlock color="green" emoji="🤮" click={changeVisibility}/>	
-				<ColorBlock color="purple" emoji="😴" click={changeVisibility}/>	
-				<ColorBlock color="orange" emoji="😳" click={changeVisibility}/>	
+				<ColorBlock color="green" emoji="🤮" click={changeVisibility} send_vote={AddItem}/>	
+				<ColorBlock color="purple" emoji="😴" click={changeVisibility} send_vote={AddItem}/>	
+				<ColorBlock color="orange" emoji="😳" click={changeVisibility} send_vote={AddItem}/>	
 			</div>
 
 			</div>
     	</div>
 			<div className="results" style={{display: showResults}}>
 			<p style={{fontSize:'24px'}}>Results</p>
-			<Pie data={data} options={options} />
+			<Pie data={chart_data} options={options} />
 		</div>
 	
 		</div>
